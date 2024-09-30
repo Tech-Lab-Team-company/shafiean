ca1536752d500c6461a97266a7325d6a15ec3af0
KayanIbraHem <kioghubacc@gmail.com>
2024-09-30 11:19:35 +0300

    gdf

--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\User;
 
 use App\Enum\UserTypeEnum;
 use App\Enum\IdentityTypeEnum;
+use Illuminate\Validation\Rule;
 use App\Helpers\Response\ApiRequest;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -24,7 +25,7 @@ class StoreUserRequest extends ApiRequest
             'gender' => 'nullable|string|max:10',
             'api_key' => 'nullable|string|max:255',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'date_of_birth' => 'nullable|date|date_format:Y-m-d',
+            'date_of_birth' => 'nullable|date|date_format:Y-m-d', 
             'identity_type' => 'nullable|numeric|in:' . enumCaseValue(IdentityTypeEnum::class),
             'type' => ['nullable', 'numeric', 'in:' . enumCaseValue(UserTypeEnum::class)],
             'identity_number' => 'nullable|string|max:255',
@@ -32,7 +33,7 @@ class StoreUserRequest extends ApiRequest
             'blood_type_id' => 'nullable|exists:blood_types,id',
             'country_id' => 'nullable|exists:countries,id',
             'group_ids' => 'nullable|array',
-            'group_ids.*' => ['nullable',R],
+            'group_ids.*' => ['nullable', Rule::exists('groups', 'id')->whereNull('deleted_at')],
         ];
     }
 }
