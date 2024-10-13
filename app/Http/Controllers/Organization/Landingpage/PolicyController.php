4da573cb34e86484481eb278691f7d91a3aa3fdd
KayanIbraHem <kioghubacc@gmail.com>
2024-10-13 16:28:09 +0300

    Policy LandingPage

---
+++ app/Http/Controllers/Organization/Landingpage/PolicyController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Organization\Landingpage;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\Organization\Landingpage\Policy\PolicyService;
+use App\Http\Requests\Organization\Landingpage\Policy\StorePolicyRequest;
+use App\Http\Requests\Organization\Landingpage\Policy\DeletePolicyRequest;
+use App\Http\Requests\Organization\Landingpage\Policy\UpdatePolicyRequest;
+use App\Http\Requests\Organization\Landingpage\Policy\FetchPolicyDetailsRequest;
+
+class PolicyController extends Controller
+{
+    public function __construct(protected  PolicyService $policyService) {}
+    public function index()
+    {
+        return $this->policyService->index()->response();
+    }
+    public function show(FetchPolicyDetailsRequest $request)
+    {
+        return $this->policyService->show($request)->response();
+    }
+    public function store(StorePolicyRequest $request)
+    {
+        return $this->policyService->store($request)->response();
+    }
+    public function update(UpdatePolicyRequest $request)
+    {
+        return $this->policyService->update($request)->response();
+    }
+    public function delete(DeletePolicyRequest $request)
+    {
+        return $this->policyService->delete($request)->response();
+    }
+}
---
+++ app/Models/Organization/Landingpage/Policy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Organization\Landingpage;
+
+use App\Models\Organization;
+use Illuminate\Database\Eloquent\Model;
+use App\Observers\OrganizationIdObserver;
+use App\Models\Scopes\PerOrganizationScope;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Policy extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new PerOrganizationScope);
+    }
+    protected static function boot()
+    {
+        parent::boot();
+        static::observe(OrganizationIdObserver::class);
+    }
+}
