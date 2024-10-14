a370f54a25dc5adfc1b554a684b93a9fda5d1c9d
KayanIbraHem <kioghubacc@gmail.com>
2024-10-14 14:17:22 +0300

    add landingpage end point in api route

--- routes/Api/api.php
+++ routes/Api/api.php
@@ -1,9 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Global\CityController;
 use App\Http\Controllers\Global\CountryController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchBlogController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchHeaderController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchPolicyController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchScreenController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchOpinionController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchPartnerController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchPrivacyController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchServiceController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchStatisticController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchSubHeaderController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchCompetitionController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchCommonQuestionController;
+use App\Http\Controllers\Organization\Landingpage\EndPoint\FetchServiceFeatureController;
 
 
 Route::post('user/login', [AuthController::class, 'login']);
@@ -28,3 +41,36 @@ Route::post('add_city', [CityController::class, 'store'])->name('cities.store');
 Route::post('fetch_city_details', [CityController::class, 'show'])->name('cities.show');
 Route::post('edit_city', [CityController::class, 'update'])->name('cities.update');
 Route::post('delete_city', [CityController::class, 'destroy'])->name('cities.destroy');
+/**
+ * lANDINGPAGE END POINT START
+ */
+
+//HEADER
+Route::get('landing_page_fetch_headers', FetchHeaderController::class);
+//SUB HEADER
+Route::get('landing_page_fetch_sub_headers', FetchSubHeaderController::class);
+//STATISTIC
+Route::get('landing_page_fetch_Statistics', FetchStatisticController::class);
+//PARTNER
+Route::get('landing_page_fetch_partners', FetchPartnerController::class);
+//SERVICE
+Route::get('landing_page_fetch_services', FetchServiceController::class);
+//SERVICE FEATURE
+Route::get('landing_page_fetch_service_features', FetchServiceFeatureController::class);
+//OPINION
+Route::get('landing_page_fetch_opinions', FetchOpinionController::class);
+//SCREEN
+Route::get('landing_page_fetch_screens', FetchScreenController::class);
+//COMPETITION
+Route::get('landing_page_fetch_competitions', FetchCompetitionController::class);
+//BLOG
+Route::get('landing_page_fetch_blogs', FetchBlogController::class);
+//COMMON QUESTION
+Route::get('landing_page_fetch_common_questions', FetchCommonQuestionController::class);
+//PRIVACY
+Route::get('landing_page_fetch_privacy', FetchPrivacyController::class);
+//POLICY
+Route::get('landing_page_fetch_policy', FetchPolicyController::class);
+/**
+ * lANDINGPAGE END POINT END
+ */
