d6ab27a2e98545021ff09c7330df923cb9f7d0fb
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2024-10-15 16:28:04 +0300

    document organization scope to orginal

--- app/Models/Scopes/PerOrganizationScope.php
+++ app/Models/Scopes/PerOrganizationScope.php
@@ -2,11 +2,13 @@
 
 namespace App\Models\Scopes;
 
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class PerOrganizationScope implements Scope
 {
@@ -16,8 +18,14 @@ class PerOrganizationScope implements Scope
     public function apply(Builder $builder, Model $model): void
     {
         $table = $model->getTable();
+        /* $organization_id = null;
+        if(FacadesAuth::check()){
+            $organization_id = get_auth_organization_id();
+        }else{
+            $organization_id = get_organization_id_for_website(request());
+        } */
         if (Schema::hasColumn($table, 'organization_id')) {
-            $builder->where('organization_id', get_auth_organization_id());
+            $builder->where('organization_id',get_auth_organization_id());
         }
     }
 }
