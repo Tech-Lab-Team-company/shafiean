ac8f3c1996c38648d75fae8377aaa8d2195507dd
KayanIbraHem <kioghubacc@gmail.com>
2024-10-27 15:57:20 +0300

    user_fetch_library_details

--- app/Http/Controllers/User/Library/FetchLibraryDetailsController.php
+++ app/Http/Controllers/User/Library/FetchLibraryDetailsController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers\User\Library;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\User\Library\FetchLibraryDetailsService;
-use App\Http\Requests\Organization\Library\FetchLibraryDetailsRequest;
+use App\Http\Requests\User\Library\UserFetchLibraryDetailsRequest;
 
 class FetchLibraryDetailsController extends Controller
 {
     public function __construct(protected  FetchLibraryDetailsService $fetchLibraryDetailsService) {}
-    public function show(FetchLibraryDetailsRequest $request)
+    public function show(UserFetchLibraryDetailsRequest $request)
     {
         return $this->fetchLibraryDetailsService->show($request)->response();
     }
--- app/Services/User/Library/FetchLibraryDetailsService.php
+++ app/Services/User/Library/FetchLibraryDetailsService.php
@@ -3,13 +3,9 @@
 namespace App\Services\User\Library;
 
 use Exception;
-use App\Helpers\Response\DataFailed;
-use App\Helpers\Response\DataStatus;
-use Illuminate\Support\Facades\File;
 use App\Helpers\Response\DataSuccess;
-use Illuminate\Support\Facades\Storage;
 use App\Models\Organization\Library\Library;
-use App\Http\Resources\Organization\Library\LibraryResource;
+use App\Http\Resources\User\Library\FetchLibraryDetailsResource;
 
 class FetchLibraryDetailsService
 {
@@ -17,7 +13,7 @@ class FetchLibraryDetailsService
     {
         $library = Library::whereId($request->id)->first();
         return new DataSuccess(
-            data: new LibraryResource($library),
+            data: new FetchLibraryDetailsResource($library),
             statusCode: 200,
             message: 'Fetch Library Details successfully'
         );
