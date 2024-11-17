3e3b0cb0c11298db5e15bf06a1c3d77a9a2c5a1a
AhmedWeb2022 <ahmedwry588@gmail.com>
2024-11-17 16:33:09 +0200

    edit

---
+++ app/Http/Controllers/Parent/Child/ChildController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Parent\Child;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChildController extends Controller
+{
+    //
+}
--- routes/Parent/parent.php
+++ routes/Parent/parent.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\Parent\Child\ChildController;
 use App\Http\Controllers\User\Session\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:user')->group(function () {
     Route::controller(SessionController::class)->group(function () {
         Route::post('fetch_student_sessions', 'fetch_student_sessions');
+    });
+
+    Route::controller(ChildController::class)->group(function () {
+        
     });
 });
