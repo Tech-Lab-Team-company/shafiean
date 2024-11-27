647d6d43343b398602e2aa2c383d59b26eb7fb76
KayanIbraHem <kioghubacc@gmail.com>
2024-11-27 11:38:27 +0200

    add_employee

--- app/Http/Resources/OrganizationEmployeeResource.php
+++ app/Http/Resources/OrganizationEmployeeResource.php
@@ -19,24 +19,24 @@ class OrganizationEmployeeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'image' => $this->image_link,
-            'age' => $this->age,
-            'gender' => $this->gender,
-            'phone' => $this->phone,
-            'is_master' => $this->is_master,
-            'organization_id' => $this->organization_id,
-            'marital_status' => $this->marital_status,
-            'identity_type' => $this->identity_type,
-            'identity_number' => $this->identity_number,
-            'date_of_birth' => $this->date_of_birth,
-            'is_employed' => $this->is_employed,
-            'status' => EmployeeTypeEnum::from($this->is_employed)->label(),
+            'id' => $this->id ?? 0,
+            'name' => $this->name ?? "",
+            'email' => $this->email ?? "",
+            'image' => $this->image_link ?? "",
+            'age' => $this->age ?? "",
+            'gender' => $this->gender ?? "",
+            'phone' => $this->phone ?? "",
+            'is_master' => $this->is_master ?? "",
+            'organization_id' => $this->organization_id ?? "",
+            'marital_status' => $this->marital_status ?? "",
+            'identity_type' => $this->identity_type ?? "",
+            'identity_number' => $this->identity_number ?? "",
+            'date_of_birth' => $this->date_of_birth ?? "",
+            'is_employed' => $this->is_employed ?? "",
+            'status' => EmployeeTypeEnum::from($this->is_employed)->label() ?? "",
             'curriculums' =>  CurriculumResource::collection($this->curriculums ?? []) ?? [],
             'images' => $this->images()->count() > 0 ? ImageResource::collection($this->images) : [],
-            'job_type' => new JobTypeResource($this->jobType)
+            'job_type' => new JobTypeResource($this->jobType) ?? ""
         ];
     }
 }
