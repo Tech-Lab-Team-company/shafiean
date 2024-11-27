aba465dd27aa1e4f5d712d657cd34ea0de6a395c
AhmedWeb2022 <ahmedwry588@gmail.com>
2024-11-27 16:41:53 +0200

    edit

--- app/Services/Organization/Group/GroupService.php
+++ app/Services/Organization/Group/GroupService.php
@@ -78,8 +78,12 @@ class GroupService
                     $group->days()->attach($day['day_id'], ['start_time' => $day['start_time'], 'end_time' => $day['end_time']]);
                 }
             }
-            if ($request->with_all_disability == HasDisabilityEnum::HAS_DISABILITY->value && $request->disabilities) {
+            if ($request->with_all_disability == HasDisabilityEnum::NOT_HAS_DISABILITY->value && $request->disabilities) {
                 $group->disabilities()->attach($request->disabilities, ['course_id' => $request->course_id]);
+            }
+            if ($request->with_all_disability == HasDisabilityEnum::HAS_DISABILITY->value) {
+                $course_disapility_ids = Course::find($request->course_id)->disability_types->pluck('id')->toArray();
+                $group->disabilities()->attach($course_disapility_ids, ['course_id' => $request->course_id]);
             }
             if ($request->with_all_course_content == HasCourseEnum::HAS_COURSE->value) {
                 $courseStages = Course::find($request->course_id)->stages;
