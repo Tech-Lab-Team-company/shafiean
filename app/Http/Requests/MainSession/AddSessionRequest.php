a35351d3b266b3a0da6405dfc41de96758c84672
KayanIbraHem <kioghubacc@gmail.com>
2024-12-10 17:02:31 +0200

    Ggdf

--- app/Http/Requests/MainSession/AddSessionRequest.php
+++ app/Http/Requests/MainSession/AddSessionRequest.php
@@ -7,19 +7,10 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class AddSessionRequest extends ApiRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
