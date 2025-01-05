d2f8dbd60f8835b7a201c264e5f3a05cfa757c2e
KayanIbraHem <kioghubacc@gmail.com>
2025-01-05 11:41:21 +0200

    AdminShowRequest authorize true

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -33,6 +33,7 @@ class AdminController extends Controller
 
     public function show(AdminShowRequest $request)
     {
+        dd($request->all());
         return $this->adminService->getById($request)->response();
     }
 
