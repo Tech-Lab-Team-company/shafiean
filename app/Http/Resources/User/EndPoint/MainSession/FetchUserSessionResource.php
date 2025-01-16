9cd39e0283d49cc4a9dd9e43a38f66ee90c111bf
KayanIbraHem <kioghubacc@gmail.com>
2025-01-16 10:40:20 +0200

    gdf

--- app/Http/Resources/User/EndPoint/MainSession/FetchUserSessionResource.php
+++ app/Http/Resources/User/EndPoint/MainSession/FetchUserSessionResource.php
@@ -18,7 +18,7 @@ class FetchUserSessionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        dd($this->session);
+        dd($this);
         $isLive = $this->lives()->whereLeaveDate(null)->count() > 0 ? true : false;
         return [
             'id' => $this->id ?? 0,
