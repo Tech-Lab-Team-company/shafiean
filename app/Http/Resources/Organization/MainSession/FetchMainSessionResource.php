f97983ed49d7f6a469f394118e75d57eb38fac6a
KayanIbraHem <kioghubacc@gmail.com>
2025-01-16 11:32:24 +0200

    gfd

--- app/Http/Resources/Organization/MainSession/FetchMainSessionResource.php
+++ app/Http/Resources/Organization/MainSession/FetchMainSessionResource.php
@@ -21,8 +21,8 @@ class FetchMainSessionResource extends JsonResource
         return [
             'id' => $this->id ?? 0,
             'title' => $isSession ? $this->session->title : $this->title,
-            'start_verse' => $isSession ? (int) $this->session->startAyah : $this->startAyah->text,
-            'end_verse' => $isSession ? (int) $this->session->endAyah : $this->endAyah->text,
+            'start_verse' => $isSession ?  $this->session->startAyah : $this->startAyah->text,
+            'end_verse' => $isSession ?  $this->session->endAyah : $this->endAyah->text,
             "teacher_name" => $this->teacher->name ?? "",
 
             "stage" => new StageResource($this->stage) ?? "",
