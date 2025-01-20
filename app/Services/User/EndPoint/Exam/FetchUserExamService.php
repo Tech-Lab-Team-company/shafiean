33fe93ffd71ea5f655ad438636ab6eca102c098b
KayanIbraHem <kioghubacc@gmail.com>
2025-01-20 14:37:11 +0200

    remove dead code

--- app/Services/User/EndPoint/Exam/FetchUserExamService.php
+++ app/Services/User/EndPoint/Exam/FetchUserExamService.php
@@ -24,17 +24,16 @@ class FetchUserExamService
     public function fetchUserExam($dataRequest): DataStatus
     {
         try {
+            /** @var User $user  */
             $user = auth('user')->user();
             $completedExams = ExamResult::where('user_id', $user->id)
                 ->pluck('exam_id')
                 ->toArray();
-            // dd($completedExams , 'examresult');
             if ($dataRequest->group_id) {
                 $examGroups = ExamGroup::where('group_id', $dataRequest->group_id)->pluck('exam_id')->toArray();
             } else {
                 $groups =   $user->subscripe_groups()->pluck('group_id')->toArray();
                 $examGroups = ExamGroup::whereIn('group_id', $groups)->pluck('exam_id')->toArray();
-                // dd($examGroups , 'exam groups');
             }
             $exams = Exam::whereIn('id', $examGroups)
                 ->whereNotIn('id', $completedExams)
