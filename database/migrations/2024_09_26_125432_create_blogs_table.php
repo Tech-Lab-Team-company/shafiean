50d9292479d78028d8bbe09aa94c0dff971f1136
KayanIbraHem <kioghubacc@gmail.com>
2025-01-21 12:26:00 +0200

    longText

--- database/migrations/2024_09_26_125432_create_blogs_table.php
+++ database/migrations/2024_09_26_125432_create_blogs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('organization_id')->nullable()->references('id')->on('organizations')->onDelete('cascade');
             $table->string('title')->nullable();
-            $table->string('description')->nullable();
+            $table->longText('description')->nullable();
             $table->string('image')->nullable();
             $table->softDeletes();
             $table->timestamps();
