ffccc22918b80640c16d4bf9b61fc3ac6d005579
KayanIbraHem <kioghubacc@gmail.com>
2025-01-29 11:57:23 +0200

    remove dead code

--- app/Http/Resources/Organization/MainSession/FetchMainSessionDetailsForGroupResource.php
+++ app/Http/Resources/Organization/MainSession/FetchMainSessionDetailsForGroupResource.php
@@ -23,13 +23,7 @@ class FetchMainSessionDetailsForGroupResource extends JsonResource
         return [
             'id' => $this->id ?? 0,
             'title' => $this->session_id ? $this->session->title : $this->title,
-            // 'stage_title' => $this->stage->title ?? "",
-            // 'session_type' => $this->session_type->title ?? "",
             'curriculum_title' => $this->stage->curriculum->title ?? "",
-            // 'surah_title' => $this->surah->name ?? "",
-            // 'start_ayah_title' => $this->startAyah->text ?? "",
-            // 'end_ayah_title' => $this->endAyah->text ?? "",
-            // 'teacher_name' => $this->teacher->name ?? "",
             'date' => $this->date ?? "",
             'start_time' => $this->start_time ?? "",
             'end_time' => $this->end_time ?? "",
