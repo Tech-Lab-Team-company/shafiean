ffe1fb0066e52dde65fcb02d3b5c4ffedfeea010
KayanIbraHem <kioghubacc@gmail.com>
2025-02-04 17:00:26 +0200

    search

--- app/Services/Teacher/Group/TeacherGroupService.php
+++ app/Services/Teacher/Group/TeacherGroupService.php
@@ -119,24 +119,16 @@ class TeacherGroupService
         try {
             /** @var Teacher $teacher  */
             $teacher = Auth::guard('organization')->user();
-            $exams = $teacher->sessions()->with('group.exams')->get()->pluck('group.exams')->flatten()->unique();
-            // $query = $teacher->sessions()->with(['group.exams' => function ($q) use ($dataRequest) {
-            //     if ($dataRequest->filled('word')) {
-            //         $q->where('name', 'like', '%' . $dataRequest->word . '%');
-            //     }
-            // }]);
-            // $exams = $query->get()
-            //     ->pluck('group.exams')
-            //     ->flatten()
-            //     ->unique();
-            // إنشاء استعلام للامتحانات المرتبطة بالمجموعات الخاصة بالمعلم
-            // $exams = $teacher->sessions()
-            //     ->join('groups', 'sessions.group_id', '=', 'groups.id') // Join with the groups table
-            //     ->join('exams', 'groups.id', '=', 'exams.group_id') // Join with the exams table
-            //     ->select('exams.*') // Select only the columns from the exams table
-            //     ->distinct() // Ensure unique results
-            //     ->paginate(10);
-
+            // $exams = $teacher->sessions()->with('group.exams')->get()->pluck('group.exams')->flatten()->unique();
+            $query = $teacher->sessions()->with(['group.exams' => function ($q) use ($dataRequest) {
+                if ($dataRequest->filled('word')) {
+                    $q->where('name', 'like', '%' . $dataRequest->word . '%');
+                }
+            }]);
+            $exams = $query->get()
+                ->pluck('group.exams')
+                ->flatten()
+                ->unique();
             return new DataSuccess(
                 data: TeacherGroupExamResource::collection($exams),
                 status: true,
