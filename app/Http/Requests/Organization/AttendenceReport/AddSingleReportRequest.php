a5afc8fc4b7d99cad2563ee9a4eb0ebef047b52b
Mostafa-tarek <mostafa.tarek22555@gmail.com>
2025-09-10 10:36:12 +0300

    add notes in request

--- app/Http/Requests/Organization/AttendenceReport/AddSingleReportRequest.php
+++ app/Http/Requests/Organization/AttendenceReport/AddSingleReportRequest.php
@@ -24,6 +24,7 @@ class AddSingleReportRequest extends ApiRequest
             ],
             'is_absent' => 'required|boolean',
             'report_id' => 'nullable|exists:reports,id',
+            'notes' => 'nullable|string',
         ];
     }
 }
