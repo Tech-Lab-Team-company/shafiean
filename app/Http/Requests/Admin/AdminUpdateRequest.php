8211b4451bc394ea6b403f9c6b8f3b56e9227833
Mostafa-tarek <mostafa.tarek22555@gmail.com>
2025-09-14 10:26:15 +0300

    handle request

--- app/Http/Requests/Admin/AdminUpdateRequest.php
+++ app/Http/Requests/Admin/AdminUpdateRequest.php
@@ -8,6 +8,11 @@ use Illuminate\Validation\Rule;
 
 class AdminUpdateRequest extends ApiRequest
 {
+    protected $id;
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
     public function authorize()
     {
         return true;
